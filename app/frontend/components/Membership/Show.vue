0c3a283cfaa6edbf9224c074271617c225586a44
Roman Ciobanu <roman.ciobanu@saltedge.com>
2026-01-08 16:21:14 +0200

    [membership] add show page

---
+++ app/frontend/components/Membership/Show.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="mt-4 p-5 bg-light text-black rounded --bs-gray-500">
+        <h1> Afilierea {{membership.id}}</h1>
+        <p><strong>Sportiv: </strong><a :href="`/runners/${membership.runner_id}`">{{membership.runner?.runner_name}} {{membership.runner?.surname}}</a></p>
+        <p><strong>Club: </strong><a :href="`/clubs/${membership.club_id}`">{{membership.club?.club_name}}</a></p>
+        <p><strong>Numarul de rezultate: </strong><a :href="`/results?membership=${membership.id}`">{{results.length}}</a></p>
+    </div>
+    <p class="d-inline-flex gap-1">
+        <a class="btn btn-primary" data-bs-toggle="collapse" href="#resultsTable" role="button" aria-expanded="false" aria-controls="resultsTable">
+            Rezultate
+        </a>
+    </p>
+    <div class="collapse" id="resultsTable">
+        <div class="card card-body">
+            <table class="table table-striped table-bordered table-hover">
+                <thead class="table-primary">
+                    <tr>
+                        <td @click="orderResultTable('place')">Locul</td>
+                        <td @click="orderResultTable('full_name')">Sportiv</td>
+                        <td @click="orderResultTable('club_name')">Club</td>
+                        <td @click="orderResultTable('runner_category_name')">Categoria actuala</td>
+                        <td @click="orderResultTable('time')">Timpul</td>
+                        <td @click="orderResultTable('result_category_name')">Categoria Indeplinita</td>
+                        <td @click="orderResultTable('status')">Indeplinire</td>
+                        <td @click="orderResultTable('date')">Data</td>
+                        <td @click="orderResultTable('competition_name')">Competitia</td>
+                        <td @click="orderResultTable('group_name')">Grupa</td>
+                        <td @click="orderResultTable('wre_points')">WRE Puncte</td>
+                        <td @click="orderResultTable('ecn_points')">ECN Puncte</td>
+                        <th colspan="3">Actiuni</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="element in results" :key="element.id">
+                        <td>{{element.place}}</td>
+                        <td><a :href="`/runners/${element.runner_id}`">{{element.full_name}}</a></td>
+                        <td><a :href="`/clubs/${element.club_id}`">{{element.club_name}}</a></td>
+                        <td><a :href="`/categories/${element.runner_category_id}`">{{element.runner_category_name}}</a></td>
+                        <td>{{formatResultTime(element.time)}}</td>
+                        <td><a :href="`/categories/${element.result_category_id}`">{{element.result_category_name}}</a></td>
+                        <td>{{element.status}}</td>
+                        <td>{{element.date}}</td>
+                        <td><a :href="`/competitions/${element.competition_id}`">{{element.competition_name}}</a></td>
+                        <td><a :href="`/groups/${element.group_id}`">{{element.group_name}}</a></td>
+                        <td>{{element.wre_points}}</td>
+                        <td>{{element.ecn_points}}</td>
+                        <td>
+                            <a class="btn btn-sm btn-warning" :href="`/results/${element.id}`">
+                                Arată
+                            </a>
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-success" @click="editElement(element)">
+                                Editează
+                            </button>
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-danger" @click="deleteElement(element.id)">
+                                Șterge
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <p class="lead">
+        <button class="btn btn-sm btn-success" @click="editCategory(category)">Editeaza</button>
+        <button class="btn btn-danger btn-sm" @click="deleteCategory(category.id)">Sterge</button>
+    </p>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+const membership = ref({})
+const membershipId = ref("")
+const results = ref([])
+const resultSorting = ref({
+    "sorting[sort_by]": "date",
+    "sorting[direction]": "desc"
+})
+
+onMounted(() => {
+    membershipId.value = window.location.pathname.split('/').pop();
+    getData();
+    getResults();
+})
+
+async function getData() {
+    const res = await axios.get(`/memberships/${membershipId.value}.json`)
+    membership.value = res.data
+}
+
+async function getResults() {
+    resultSorting.value["membership"] = membershipId.value
+    const res = await axios.get('/results.json', { params: resultSorting.value })
+    results.value = res.data;
+}
+
+function formatResultTime(seconds) {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+
+    return (
+        String(h).padStart(2, '0') + ':' +
+        String(m).padStart(2, '0') + ':' +
+        String(s).padStart(2, '0')
+    );
+}
+</script>
--- app/frontend/entrypoints/memberships.js
+++ app/frontend/entrypoints/memberships.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import MembershipsIndex from '../components/Membership/Index.vue'
+import MembershipsShow from '../components/Membership/Show.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const el_index = document.getElementById('memberships_index')
   if (el_index) {
     createApp(MembershipsIndex).mount(el_index)
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el_show = document.getElementById('memberships_show')
+  if (el_show) {
+    createApp(MembershipsShow).mount(el_show)
   }
 })
