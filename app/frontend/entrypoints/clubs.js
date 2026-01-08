d3433966e0b7a391bbd4544b59c9c5b396ab7e55
Roman Ciobanu <roman.ciobanu@saltedge.com>
2026-01-08 15:32:44 +0200

    [club] add show page

--- app/frontend/entrypoints/clubs.js
+++ app/frontend/entrypoints/clubs.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import ClubsIndex from '../components/Club/Index.vue'
+import ClubsShow from '../components/Club/Show.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const el_index = document.getElementById('clubs_index')
   if (el_index) {
     createApp(ClubsIndex).mount(el_index)
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el_show = document.getElementById('clubs_show')
+  if (el_show) {
+    createApp(ClubsShow).mount(el_show)
   }
 })
