7697bbca4d63e4f07bd0f630adbc9805085b41ee
Roman Ciobanu <roman.ciobanu@saltedge.com>
2026-01-09 15:47:16 +0200

    add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.html # renders index.html.erb
+      format.json { render json:
+        {
+          runners_count:      Runner.count,
+          competitions_count: Competition.count,
+          clubs_count:        Club.count,
+          results_count:      Result.count
+        }
+      }
+    end
   end
 end
